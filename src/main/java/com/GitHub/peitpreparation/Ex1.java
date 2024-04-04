ec1b4bd5e9e9d7be7a17e74394534d709ec28f0a
Keerthi <nunnakeerthi488@gmail.com>
2024-04-04 17:27:30 +0530

    adding ex3

--- src/main/java/com/GitHub/peitpreparation/Ex1.java
+++ src/main/java/com/GitHub/peitpreparation/Ex1.java
@@ -3,6 +3,7 @@ package com.GitHub.peitpreparation;
 public class Ex1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GITHUB");
+		
 	}
 
 }
---
+++ src/main/java/com/GitHub/peitpreparation/Ex3.java
@@ -0,0 +1,14 @@
+package com.GitHub.peitpreparation;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		int k=15;
+		int n=i+k;
+		System.out.println(n);
+
+	}
+
+}
